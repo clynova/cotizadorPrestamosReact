d1b814ad40694a40c026e85de1a55e95a01b2b09
Clynova <clynova.dev@gmail.com>
2025-01-24 13:19:13 -0300

    Se crean stat para la cantidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
+import { Header } from "./components/Header"
 
 function App() {
 
+  const [cantidad, setCantidad] = useState(10000);
+
   return (
-    <p>Hola mundo</p>
+    <div className="my-2 max-w-lg mx-auto bg-white shadow p-10">
+      <Header />
+
+      <input 
+        type="range" 
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      />
+
+
+    </div>
   )
 }
 
